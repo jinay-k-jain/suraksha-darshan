d1c8279caa37ac6df5d235566eceef63ea8553fe
Jinay Jain <Jinayjain56@gmail.com>
2025-12-19 11:00:26 +0530

    Delete frontend directory

--- frontend/src/hooks/useTranslation.js
+++
@@ -1,9 +0,0 @@
-import { useBooking } from '../context/BookingContext'
-import { translate } from '../i18n/translations'
-
-const useTranslation = () => {
-  const { language } = useBooking()
-  return (key, fallback) => translate(key, language) || fallback || key
-}
-
-export default useTranslation
